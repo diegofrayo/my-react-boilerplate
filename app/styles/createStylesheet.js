394a5b03e38682fc72d26c672af53a433f3b0745
Diego Rayo:q <diegofrayo@gmail.com>
2017-12-18 13:52:41 -0500

    [update] styles files

--- app/styles/createStylesheet.js
+++ app/styles/createStylesheet.js
@@ -1,3 +1,7 @@
+import chroma from 'chroma-js';
+
+const tones = [100, 200, 300, 400, 500, 600, 700];
+
 export const theme = {
   spacing: {
     base: 10,
@@ -7,12 +11,57 @@ export const theme = {
     large: 20,
   },
   color: {
-    body: '#FEFEFE',
-    backgroundPrimary: '#EAEAEA',
-    backgroundSecondary: '#CACACA',
-    textPrimary: '#323232',
-    textSecondary: '#878787',
-    themeColor1: '',
+    body: '#FFF',
+    black: tones.reduce((acum, current, index) => {
+      // eslint-disable-next-line
+      acum[current] = chroma('#555')
+        .darken((index + 1) * 0.2)
+        .hex();
+      return acum;
+    }, {}),
+    backgroundPrimary: {
+      base: '#FFF',
+      ...tones.reduce((acum, current, index) => {
+        // eslint-disable-next-line
+        acum[current] = chroma('#FFF')
+          .darken((index + 1) * 0.2)
+          .hex();
+        return acum;
+      }, {}),
+    },
+    backgroundSecondary: {
+      base: '#CCC',
+      ...tones.reduce((acum, current, index) => {
+        // eslint-disable-next-line
+        acum[current] = chroma('#CCC')
+          .darken((index + 1) * 0.2)
+          .hex();
+        return acum;
+      }, {}),
+    },
+    textPrimary: {
+      base: '#111',
+      ...tones.reduce((acum, current, index) => {
+        // eslint-disable-next-line
+        acum[current] = chroma('#222')
+          .darken((index + 1) * 0.2)
+          .hex();
+        return acum;
+      }, {}),
+    },
+    textSecondary: {
+      base: '#555',
+      ...tones.reduce((acum, current, index) => {
+        // eslint-disable-next-line
+        acum[current] = chroma('#666')
+          .darken((index + 1) * 0.2)
+          .hex();
+        return acum;
+      }, {}),
+    },
+    themeColor1: {
+      base: '#7ABD64',
+    },
     themeColor2: '',
   },
   headerHeight: 50,
